106474d4b4002664742cbe13605e5045c7095beb
DvEyZ <szymonkwi@outlook.com>
2022-12-30 15:32:18 +0100

    usunięto:        test/main.cpp

--- test/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "../src/joyterm.hpp"
-
-using namespace joyterm::style;
-
-int main()
-{
-    joyterm::init();
-    std::cout << MStyle({FgColor::blue, BgColor::red, Style::bold}) << "Hello World!" << MStyle({Style::reset}) << "\n";
-}
