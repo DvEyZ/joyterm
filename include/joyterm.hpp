dc9bf46495f17a510a62f29eceef42d0ecfd6517
DvEyZ <szymonkwi@outlook.com>
2023-02-14 14:37:58 +0100

    zmieniono:       include/joyterm.hpp

--- include/joyterm.hpp
+++ include/joyterm.hpp
@@ -28,33 +28,24 @@ namespace joyterm
     namespace style
     {
         // Represents foreground (text) colors.
-        struct FgColor
+        enum class FgColor
         {
-            enum fc
-            {
-                black = 30, red = 31, green = 32, yellow = 33, blue = 34, magenta = 35, cyan = 36, white = 37, _default = 39,
-                b_black = 90, b_red = 91, b_green = 92, b_yellow = 93, b_blue = 94, b_magenta = 95, b_cyan = 96, b_white = 97
-            };
+            black = 30, red = 31, green = 32, yellow = 33, blue = 34, magenta = 35, cyan = 36, white = 37, _default = 39,
+            b_black = 90, b_red = 91, b_green = 92, b_yellow = 93, b_blue = 94, b_magenta = 95, b_cyan = 96, b_white = 97
         };
 
         // Represents background colors.
-        struct BgColor
+        enum class BgColor
         {
-            enum bc
-            {
-                black = 40, red = 41, green = 42, yellow = 43, blue = 44, magenta = 45, cyan = 46, white = 47, _default = 49,
-                b_black = 100, b_red = 101, b_green = 102, b_yellow = 103, b_blue = 104, b_magenta = 105, b_cyan = 106, b_white = 107
-            };
+            black = 40, red = 41, green = 42, yellow = 43, blue = 44, magenta = 45, cyan = 46, white = 47, _default = 49,
+            b_black = 100, b_red = 101, b_green = 102, b_yellow = 103, b_blue = 104, b_magenta = 105, b_cyan = 106, b_white = 107
         };
 
         // Represents other modifiers, such as bold or italic text, or style reset modifier.
         // Please note that not all of these modifiers are supported on all platforms.
-        struct Style
+        enum class Style
         {
-            enum style
-            {
-                reset = 0, bold = 1, dim = 2, italic = 3, underline = 4, blink = 5, inverse = 7, hidden = 8, strikethrough = 9
-            };
+            reset = 0, bold = 1, dim = 2, italic = 3, underline = 4, blink = 5, inverse = 7, hidden = 8, strikethrough = 9
         };
 
         class MStyle
